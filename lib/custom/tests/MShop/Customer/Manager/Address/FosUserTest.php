0e8d8018200d9cdf6d3c1d1a2265dc90f770ee76
Aimeos <aimeos@aimeos.org>
2017-09-15 16:59:53 +0200

    Copyright update

--- lib/custom/tests/MShop/Customer/Manager/Address/FosUserTest.php
+++ lib/custom/tests/MShop/Customer/Manager/Address/FosUserTest.php
@@ -5,7 +5,7 @@ namespace Aimeos\MShop\Customer\Manager\Address;
 
 /**
  * @license LGPLv3, http://opensource.org/licenses/LGPL-3.0
- * @copyright Aimeos (aimeos.org), 2015-2016
+ * @copyright Aimeos (aimeos.org), 2015-2017
  */
 class FosUserTest extends \PHPUnit\Framework\TestCase
 {
