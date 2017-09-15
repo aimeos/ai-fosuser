def37140fed6f184d55775227f4878ef61bdfb8f
Aimeos <aimeos@aimeos.org>
2017-09-15 17:00:03 +0200

    Make custom properties from user table easily accessible

--- lib/custom/config/mshop/customer.php
+++ lib/custom/config/mshop/customer.php
@@ -316,7 +316,7 @@ return array(
 						fos."birthday" AS "customer.birthday", fos."enabled" AS "customer.status",
 						fos."vdate" AS "customer.vdate", fos."password" AS "customer.password",
 						fos."ctime" AS "customer.ctime", fos."mtime" AS "customer.mtime",
-						fos."editor" AS "customer.editor", fos."roles", fos."salt"
+						fos."editor" AS "customer.editor", fos."roles", fos."salt", fos.*
 					FROM "fos_user" AS fos
 					:joins
 					WHERE :cond
