8f5cd20b49c05d0f95832344ffa1d3b02f5185cc
Aimeos <aimeos@aimeos.org>
2019-07-31 13:20:08 +0200

    Extend codes and types to max 64 chars

--- lib/custom/setup/default/schema/customer.php
+++ lib/custom/setup/default/schema/customer.php
@@ -119,7 +119,7 @@ return array(
 			$table->addColumn( 'id', 'integer', array( 'autoincrement' => true ) );
 			$table->addColumn( 'siteid', 'integer', [] );
 			$table->addColumn( 'domain', 'string', array( 'length' => 32 ) );
-			$table->addColumn( 'code', 'string', array( 'length' => 32 ) );
+			$table->addColumn( 'code', 'string', array( 'length' => 64 ) );
 			$table->addColumn( 'label', 'string', array( 'length' => 255 ) );
 			$table->addColumn( 'pos', 'integer', ['default' => 0] );
 			$table->addColumn( 'status', 'smallint', [] );
@@ -143,8 +143,8 @@ return array(
 			$table->addColumn( 'id', 'integer', array( 'autoincrement' => true ) );
 			$table->addColumn( 'parentid', 'integer', [] );
 			$table->addColumn( 'siteid', 'integer', [] );
-			$table->addColumn( 'key', 'string', array( 'length' => 98 ) );
-			$table->addColumn( 'type', 'string', array( 'length' => 32 ) );
+			$table->addColumn( 'key', 'string', array( 'length' => 255 ) );
+			$table->addColumn( 'type', 'string', array( 'length' => 64 ) );
 			$table->addColumn( 'domain', 'string', array( 'length' => 32 ) );
 			$table->addColumn( 'refid', 'string', array( 'length' => 32 ) );
 			$table->addColumn( 'start', 'datetime', array( 'notnull' => false ) );
@@ -174,7 +174,7 @@ return array(
 			$table->addColumn( 'id', 'integer', array( 'autoincrement' => true ) );
 			$table->addColumn( 'siteid', 'integer', [] );
 			$table->addColumn( 'domain', 'string', array( 'length' => 32 ) );
-			$table->addColumn( 'code', 'string', array( 'length' => 32 ) );
+			$table->addColumn( 'code', 'string', array( 'length' => 64 ) );
 			$table->addColumn( 'label', 'string', array( 'length' => 255 ) );
 			$table->addColumn( 'pos', 'integer', ['default' => 0] );
 			$table->addColumn( 'status', 'smallint', [] );
@@ -199,7 +199,7 @@ return array(
 			$table->addColumn( 'siteid', 'integer', [] );
 			$table->addColumn( 'parentid', 'integer', [] );
 			$table->addColumn( 'key', 'string', array( 'length' => 255 ) );
-			$table->addColumn( 'type', 'string', array( 'length' => 32 ) );
+			$table->addColumn( 'type', 'string', array( 'length' => 64 ) );
 			$table->addColumn( 'langid', 'string', array( 'length' => 5, 'notnull' => false ) );
 			$table->addColumn( 'value', 'string', array( 'length' => 255 ) );
 			$table->addColumn( 'mtime', 'datetime', [] );
