08789dc636111f212f6ea39fcf4bd442ffb2fbfc
Aimeos <aimeos@aimeos.org>
2023-10-27 09:31:42 +0200

    Restrict SQL to save and delete customer and address items

--- src/MShop/Customer/Manager/FosUser.php
+++ src/MShop/Customer/Manager/FosUser.php
@@ -464,6 +464,7 @@ class FosUser
 
 		if( $id !== null ) {
 			$stmt->bind( $idx++, $context->locale()->getSiteId() . '%' );
+			$stmt->bind( $idx++, $this->getUser()?->getSiteId() );
 			$stmt->bind( $idx, $id, \Aimeos\Base\DB\Statement\Base::PARAM_INT );
 			$billingAddress->setId( $id ); // enforce ID to be present
 		} else {
